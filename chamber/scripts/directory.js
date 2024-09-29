7ce753b2d3f259760d9cfb17cdd6d8c6b6b2f411
Christopher Mendes <titolei.bossle@gmail.com>
2024-09-29 22:07:04 +0200

    W02 Assignment: Chamber Directory Page

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,82 @@
+// Fetch the fetch("scripts/members.json"); with async/await
+const fetchMembers = async () => {
+    const response = await fetch("scripts/members.json");
+    const members = await response.json();
+    return members;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    const viewOptions = document.querySelectorAll("#view-options>button");
+
+    viewOptions.forEach(option => {
+        option.addEventListener("click", event => handleViewOptionsClick(event.target));
+    });
+
+    renderCompanies(companies);
+
+});
+
+function handleViewOptionsClick(target) {
+    const companies = document.querySelector("#companies");
+    // if target id is grid-view or list-view
+    if (target.id === "grid-view") {
+        target.classList.toggle("active");
+        document.getElementById("list-view").classList.toggle("active");
+        // remove any classes that are on the companies element
+        companies.classList = "grid";
+    } else if (target.id === "list-view") {
+        target.classList.toggle("active");
+        document.getElementById("grid-view").classList.toggle("active");
+        companies.classList = "list";
+    } else {
+        // get parent element of target until it is a button
+        while (target.nodeName !== "BUTTON") {
+            target = target.parentNode;
+        }
+        
+        handleViewOptionsClick(target);
+    }
+}
+
+function renderCompanies(companies) {
+
+    
+    fetchMembers().then(members => {
+        let companiesElement = document.querySelector("#companies");
+
+        members.forEach(member => {
+            console.log(member);
+            const companyElement = document.createElement("div");
+            companyElement.classList.add("company");
+            
+            const img = document.createElement("img");
+            img.src = member.image;
+            img.alt = member.name + " logo";
+            img.loading = "lazy";
+            img.width = 200;
+            img.height = 200;
+            companyElement.appendChild(img);
+
+            const h3 = document.createElement("h3");
+            h3.textContent = member.name;
+            companyElement.appendChild(h3);
+
+            const address = document.createElement("p");
+            address.classList.add("address");
+            address.textContent = member.address;
+            companyElement.appendChild(address);
+
+            const phone = document.createElement("p");
+            phone.classList.add("phone");
+            phone.textContent = member.phone;
+            companyElement.appendChild(phone);
+
+            const website = document.createElement("a");
+            website.href = member.website;
+            website.textContent = "Visit website";
+            companyElement.appendChild(website);
+
+            companiesElement.appendChild(companyElement);
+        });
+    });
+}
---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector("#hamburger");
+
+    hamburger.addEventListener("click", () => handleHamburgerClick());
+
+    setCurrentYear();
+    setLastModified();
+    setCurrentPageNav();
+});
+
+function setCurrentYear() {
+    let currentYear = new Date().getFullYear();
+
+    document.getElementById("currentYear").textContent = currentYear;
+}
+
+function setLastModified() {
+    let lastModified = document.lastModified;
+
+    document.getElementById("lastModified").textContent = lastModified;
+}
+
+function setCurrentPageNav() {
+    let currentPage = document.URL;
+
+    let navLinks = document.querySelectorAll("nav a");
+
+    navLinks.forEach(link => {
+        // if the href attribute of the link matches the current page
+        if (currentPage.includes(link.getAttribute("href"))) {
+            // add a class to the link to style it differently
+            link.classList.add("active");
+        }
+    });
+}
+
+function handleHamburgerClick() {
+    const nav = document.querySelector("#navigation");
+
+    const isExpanded = hamburger.getAttribute("aria-expanded") === "true";
+    
+    // Toggle the aria-expanded attribute
+    hamburger.setAttribute("aria-expanded", !isExpanded);
+    
+    // Toggle aria-hidden for screen readers
+    nav.setAttribute("aria-hidden", isExpanded);
+
+    // Toggle navigation visibility by adding/removing class
+    nav.classList.toggle("expanded");
+    nav.classList.toggle("collapsed");
+
+    // Toggle the hamburger icon
+    hamburger.textContent = isExpanded ? "☰" : "✖";
+}

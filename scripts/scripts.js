2856495a177b770faed670b71aa13bb9c6dd0014
Christopher Mendes <titolei.bossle@gmail.com>
2024-10-08 21:01:16 +0200

    HTML Modals

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -74,7 +74,7 @@ const courses = [
             'CSS',
             'JavaScript'
         ],
-        completed: false
+        completed: true
     }
 ];
 
@@ -117,15 +117,26 @@ document.addEventListener("DOMContentLoaded", () => {
         if (textContent == "All") {
             updateCourseList(courses);
         } else if (textContent == "WDD") {
-            let updatedCourses= courses.filter((course) => course.subject == "WDD");
+            let updatedCourses = courses.filter((course) => course.subject == "WDD");
 
             console.log(updatedCourses);
             updateCourseList(updatedCourses);
         } else if (textContent == "CSE") {
-            let updatedCourses= courses.filter((course) => course.subject == "CSE");
+            let updatedCourses = courses.filter((course) => course.subject == "CSE");
 
             console.log(updatedCourses);
             updateCourseList(updatedCourses);
+        }
+    });
+
+    document.getElementById("close-dialog").addEventListener("click", () => {
+        document.querySelector("#course-details dialog").close();
+    });
+
+    // When clicking on the backdrop of the dialog, close the dialog
+    document.querySelector("#course-details dialog").addEventListener("click", (event) => {
+        if (event.target === document.querySelector("#course-details dialog")) {
+            document.querySelector("#course-details dialog").close();
         }
     });
 });
@@ -143,8 +154,30 @@ function updateCourseList(courses) {
 
         if (!course.completed) {
             li.setAttribute("class", "disabled");
+        } else {
+            li.addEventListener("click", handleCourseClick.bind(null, course));
         }
 
         courseList.appendChild(li);
     });
+}
+
+function handleCourseClick(course) {
+    let courseDetailsDialog = document.querySelector("#course-details dialog");
+
+    let courseId = document.getElementById("course-id");
+    let courseTitle = document.getElementById("course-title");
+    let courseCredits = document.getElementById("course-credits");
+    let courseCertificate = document.getElementById("course-certificate");
+    let courseDescription = document.getElementById("course-description");
+    let courseTechnology = document.getElementById("course-technology");
+
+    courseId.innerText = `${course.subject} ${course.number}`;
+    courseTitle.innerText = course.title;
+    courseCredits.innerText = `Credits: ${course.credits}`;
+    courseCertificate.innerText = `Certificate: ${course.certificate}`;
+    courseDescription.innerText = course.description;
+    courseTechnology.innerText = `Technologies: ${course.technology.join(", ")}`;
+
+    courseDetailsDialog.showModal();
 }

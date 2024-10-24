9cb65b4b5044505c58cc8826694a0ed105ff706c
Christopher Mendes <titolei.bossle@gmail.com>
2024-10-24 20:14:35 +0200

    Adds navbar, footer and action buttons

--- nowphoto/scripts/script.js
+++ nowphoto/scripts/script.js
@@ -1,4 +1,51 @@
-function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('nav-active');
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const openMenuButton = document.getElementById('open-menu-button');
+    const closeMenuButton = document.getElementById('close-menu-button');
+    const menu = document.getElementById('menu');
+
+    // Open menu event
+    openMenuButton.addEventListener('click', function () {
+        openMenuButton.setAttribute('aria-expanded', 'true');
+        menu.classList.remove('menu-hidden');
+        menu.classList.add('menu-visible');
+        menu.setAttribute('aria-hidden', 'false');
+
+        // Toggle button visibility
+        openMenuButton.style.display = 'none';
+        closeMenuButton.style.display = 'block';
+    });
+
+    // Close menu event
+    closeMenuButton.addEventListener('click', function () {
+        openMenuButton.setAttribute('aria-expanded', 'false');
+        menu.classList.remove('menu-visible');
+        menu.classList.add('menu-hidden');
+        menu.setAttribute('aria-hidden', 'true');
+
+        // Toggle button visibility
+        openMenuButton.style.display = 'block';
+        closeMenuButton.style.display = 'none';
+    });
+
+    let lastScrollTop = 0; // Store the last scroll position
+
+    window.addEventListener("scroll", function () {
+        const nav = document.querySelector("nav");
+        const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+        // if menu is open, do not hide it
+        if (menu.classList.contains('menu-visible')) {
+            return;
+        }
+
+        if (currentScroll > lastScrollTop) {
+            // Scrolling down
+            nav.classList.add("nav-hidden");
+        } else {
+            // Scrolling up
+            nav.classList.remove("nav-hidden");
+        }
+
+        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For mobile or negative scrolling
+    });
+});
